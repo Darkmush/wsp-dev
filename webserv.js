adce86d2c1ed41b471212b4ef89975014b42cf83
Dara Reyahi <dara@dara.se>
2012-06-24 14:04:00 +0000

    fix mongodb connection and collection next

--- webserv.js
+++ webserv.js
@@ -1,5 +1,20 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Darkmush!\n');
-}).listen(process.env.PORT);
+var express = require('express');
+var mongo = require("mongojs");
+
+//user/pass hard coded during testing.. nothing to see anyway.
+//var dbAdr = "wsp-user:dbpassword@ds033257.mongolab.com:33257/wsp-db";
+//var collections = ["users", "reports"];
+//var db = mongo.connect(dbAdr, collections);
+
+var app = express.createServer();
+
+// Configuration
+app.configure( function() {
+});
+
+// Routes
+app.get('/', function(req, res) {
+    res.send('What do you know, det funkar ju.');
+});
+
+app.listen(process.env.PORT);

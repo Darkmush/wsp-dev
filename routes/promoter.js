68e539f0980da2c7646a67182adfcde63c4db652
Dara Reyahi <dara@ubuntu.(none)>
2012-08-03 10:34:56 +0200

    0.0.2-0 commit - auth complete & password hashed

--- routes/promoter.js
+++ routes/promoter.js
@@ -1,14 +1,18 @@
+var util = require('util');
+
 exports.home = function(req, res){
   	if(!req.session.auth){
    		res.render('home', { title: 'WeSmile Promoter' });
    		return;
   	}
-	res.render('home', { title: 'WeSmile Promoter - Logged in' });
+	res.render('promoview', { title: 'WeSmile Promoter - Logged in' });
 };
 
 exports.home_post_handler = function(req, res){
-	//TODO: Hash password
-	if(req.body.password != "hej"){
+
+	var pwstring = req.body.password || "";
+	var d = require('crypto').createHash('sha1').update(pwstring).digest('hex');
+	if(d != "065b68a1145ba11e6df44564233c2ffbe901c105"){
 		req.session.destroy();
 		res.redirect('/');
 		return;	
